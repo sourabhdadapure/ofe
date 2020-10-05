c1cfe6a2ae035638e303b1f08ff287b45e93d548
sourabhdadapure <sourabhdadapure@gmail.com>
2020-10-05 10:36:21 -0400

    updates get posts action

--- modules/posts/actions.ts
+++ modules/posts/actions.ts
@@ -5,7 +5,7 @@ export const getUserPost = (userIndex: number) => {
   return async (dispatch: Dispatch) => {
     try {
       dispatch({ type: types.GET_POSTS_LOADING });
-      const payload = await fetch(`https://jsonplaceholder.typicode.com/posts/{1
+      const payload = await fetch(`https://jsonplaceholder.typicode.com/posts/
     ${userIndex}})`);
       dispatch({ type: types.GET_POSTS_SUCCESS, payload, userIndex });
     } catch (error) {
